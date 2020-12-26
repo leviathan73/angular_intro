eb4392dc88322d9cb67353ff04c37d64291c6838
Zygmunt Wychowaniec <zygmunt.wychowaniec@gmail.com>
2020-12-26 22:29:26 +0100

    0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FirstComponent
-  ], 
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		FirstComponent,
+		HeaderComponent,
+		NavigationComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule
+	],
+	providers: [],
+	bootstrap: [HeaderComponent, NavigationComponent, AppComponent]
 })
 export class AppModule { }
